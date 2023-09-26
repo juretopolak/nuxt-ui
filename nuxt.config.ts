cd6010e006184bf68e066c58ef9b7e13c2b675d0
Jure Topolak <jure.topolak@gmail.com>
2023-09-26 14:01:56 +0200

    Install Nuxt UI

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,8 +1,12 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   modules: [
+    '@nuxt/ui',
     '@nuxtjs/eslint-module'
   ],
+  colorMode: {
+    preference: 'dark' // dark / light
+  },
   eslint: {
     fix: true
   },
